d211db5db557a7757d70047049dc787f65f5b499
SwiftyOS <craigswift13@gmail.com>
2023-08-09 13:21:48 +0200

    Added e2b protocol for running agents

--- autogpt/__main__.py
+++ autogpt/__main__.py
@@ -1,52 +1,23 @@
-#!/usr/bin/env python
-
-from autopack.errors import AutoPackError
-from autopack.get_pack import try_get_packs
-from autopack.installation import install_pack
-from autopack.pack import Pack
-from autopack.selection import select_packs
+import agent_protocol
+from agent_protocol import router
 from dotenv import load_dotenv
-from langchain.agents import initialize_agent, AgentType
-from langchain.chat_models import ChatOpenAI
+from fastapi import APIRouter, Response
 
-load_dotenv()
+import autogpt.agent
 
-
-def install_packs(pack_ids: list[str]) -> list[Pack]:
-    packs = []
-    for pack_id in pack_ids:
-        try:
-            packs.append(install_pack(pack_id, force_dependencies=True))
-        except AutoPackError as e:
-            print(f"Pack {pack_id} could not be installed, leaving it out of the toolset. {e}")
-            continue
-
-    return packs
+e2b_extension_router = APIRouter()
 
 
-def main():
-    print("What would you like me to do?")
-    print("> ", end="")
-    user_input = input()
+@e2b_extension_router.get("/hb")
+async def hello():
+    return Response("Agent running")
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k-0613")
 
-    pack_ids = select_packs(user_input, llm)
-    packs = try_get_packs(pack_ids)
-
-    for pack in packs:
-        # TODO: Automatically determine how to pass init args. e.g. map env variables to API key args
-        pack.init_tool()
-
-    agent_executor = initialize_agent(
-        packs,
-        llm,
-        agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-        verbose=True,
-    )
-
-    agent_executor.run(user_input)
-
+e2b_extension_router.include_router(router)
 
 if __name__ == "__main__":
-    main()
+    """Runs the agent server"""
+    load_dotenv()
+    agent_protocol.Agent.setup_agent(
+        autogpt.agent.task_handler, autogpt.agent.step_handler
+    ).start(port=8915, router=e2b_extension_router)
