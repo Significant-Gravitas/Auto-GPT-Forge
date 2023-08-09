fb116943ec4c80bb901d33bac604dafafea47ac2
SwiftyOS <craigswift13@gmail.com>
2023-08-09 13:52:05 +0200

    update main

--- autogpt/__main__.py
+++ autogpt/__main__.py
@@ -1,23 +1,9 @@
-import agent_protocol
-from agent_protocol import router
 from dotenv import load_dotenv
-from fastapi import APIRouter, Response
 
 import autogpt.agent
-
-e2b_extension_router = APIRouter()
-
-
-@e2b_extension_router.get("/hb")
-async def hello():
-    return Response("Agent running")
-
-
-e2b_extension_router.include_router(router)
 
 if __name__ == "__main__":
     """Runs the agent server"""
     load_dotenv()
-    agent_protocol.Agent.setup_agent(
-        autogpt.agent.task_handler, autogpt.agent.step_handler
-    ).start(port=8915, router=e2b_extension_router)
+    autogpt.agent.start_agent(port=8915)
+
