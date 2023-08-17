39c6f88889b0acac9d04a1ad4db8fd33f2e44628
Merwane Hamadi <merwanehamadi@gmail.com>
2023-08-16 17:30:46 -0700

    Add agbenchmark routes + CI test
    Signed-off-by: Merwane Hamadi <merwanehamadi@gmail.com>

--- autogpt/__main__.py
+++ autogpt/__main__.py
@@ -2,13 +2,15 @@ import os
 
 from agent_protocol import Agent
 from dotenv import load_dotenv
-
 import autogpt.agent
 import autogpt.db
+from autogpt.benchmark_integration import add_benchmark_routes
 
 if __name__ == "__main__":
     """Runs the agent server"""
     load_dotenv()
+    router = add_benchmark_routes()
+
     database_name = os.getenv("DATABASE_STRING")
     print(database_name)
     port = os.getenv("PORT")
@@ -19,4 +21,4 @@ if __name__ == "__main__":
     agent = Agent.setup_agent(auto_gpt.task_handler, auto_gpt.step_handler)
     agent.db = database
     agent.workspace = workspace
-    agent.start(port=port)
+    agent.start(port=port, router=router)
